625dd21571bb9f10540985bbecfaa1af794464a3
SuuGarr <106730436+SuuGarr@users.noreply.github.com>
2024-08-09 12:02:33 +0200

    edit file name

--- run_pytest_tests.sh
+++ run_pytest_tests.sh
@@ -18,7 +18,7 @@ run_tests(){
 
     echo "Running on $platform with $context and options: '$options'"
 
-    python run_on_test_gh.py --suites xo,xp,xd,xt,xf,xc --platform "$platform" --ctx "$context" \
+    python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform "$platform" --ctx "$context" \
         --xo "$XOBJECTS" --xp "$XPART" --xd "$XDEPS" --xt "$XTRACK" --xf "$XFIELDS" --xm "$XMASK" --xc "$XCOLL" --branch "$WF_BRANCH" --pytest-opts "$options"
 
     python run_on_gh.py --suites xm --platform pcbe-abp-gpu001 --ctx cpu \
